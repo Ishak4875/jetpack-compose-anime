658ec1aa8b085ef47d97ac3472d0174f3e70b52b
Ishak4875 <ishakjaelani07@gmail.com>
2024-08-13 22:53:57 +0800

    add lib

---
+++ app/src/test/java/com/example/jetpackcomposeanime/main/MainViewModelTest.kt
@@ -0,0 +1,10 @@
+package com.example.jetpackcomposeanime.main
+
+import com.example.jetpackcomposeanime.core.domain.usecase.AnimeUseCase
+import org.junit.Assert.*
+import org.mockito.Mock
+
+class MainViewModelTest{
+    @Mock
+    private lateinit var animeUseCase: AnimeUseCase
+}
